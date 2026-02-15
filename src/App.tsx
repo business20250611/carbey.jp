d04ee06ea6c954ef843b5da9bceb68a7051c24b1
business20250611 <business20250611@gmail.com>
2026-02-15 10:57:55 +0900

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import WhatWeDo from './components/WhatWeDo';
@@ -25,10 +25,28 @@ const HomePage = () => (
   </>
 );
 
+function ScrollToHashElement() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }, 100);
+      }
+    }
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
       <div className="min-h-screen">
+        <ScrollToHashElement />
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
