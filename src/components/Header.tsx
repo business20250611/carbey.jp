65ce01a87222768e6b1fc4a1f54c125bfdc180eb
business20250611 <business20250611@gmail.com>
2026-03-18 14:41:59 +0900

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -69,7 +69,7 @@ const Header: React.FC = () => {
     window.scrollTo(0, 0);
     setIsMenuOpen(false);
   };
-
+#1F2F4D
   return (
     <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'shadow-lg' : ''
       }`} style={{ backgroundColor: '#1F2F4D' }}>
